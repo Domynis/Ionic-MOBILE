bd05c71485e49d27a981a8447187b058b5085c9c
Domynis <54539177+Domynis@users.noreply.github.com>
2024-11-09 15:24:20 +0200

    comment ping code on fe #2

--- ionic/src/state/useServerStatus.tsx
+++ ionic/src/state/useServerStatus.tsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from 'react';
-import { ping } from './pingApi';
+// import { useState, useEffect } from 'react';
+// import { ping } from './pingApi';
 
-export const useServerStatus = (interval = 5000) => {
-    const [isServerAvailable, setIsServerAvailable] = useState(true);
+// export const useServerStatus = (interval = 5000) => {
+//     const [isServerAvailable, setIsServerAvailable] = useState(true);
 
-    useEffect(() => {
-        const checkServerStatus = async () => {
-            try {
-                const response = await ping();
-                if (response === 'pong') {
-                    setIsServerAvailable(true);
-                } else {
-                    setIsServerAvailable(false);
-                }
-            } catch (error) {
-                // console.warn('Ping failed:', error);
-                setIsServerAvailable(false);
-            }
-        };
+//     useEffect(() => {
+//         const checkServerStatus = async () => {
+//             try {
+//                 const response = await ping();
+//                 if (response === 'pong') {
+//                     setIsServerAvailable(true);
+//                 } else {
+//                     setIsServerAvailable(false);
+//                 }
+//             } catch (error) {
+//                 // console.warn('Ping failed:', error);
+//                 setIsServerAvailable(false);
+//             }
+//         };
 
-        const intervalId = setInterval(checkServerStatus, interval);
+//         const intervalId = setInterval(checkServerStatus, interval);
 
-        // Run the check immediately on mount
-        checkServerStatus();
+//         // Run the check immediately on mount
+//         checkServerStatus();
 
-        return () => clearInterval(intervalId); // Clear interval on unmount
-    }, [interval]);
+//         return () => clearInterval(intervalId); // Clear interval on unmount
+//     }, [interval]);
 
-    return isServerAvailable;
-};
+//     return isServerAvailable;
+// };
